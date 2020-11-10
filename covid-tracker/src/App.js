14e7997fe762ba8884a8463d658a195a3d75c86a
MoreKodhek <morekodhek@gmail.com>
2020-11-10 14:59:28 +0300

    Second commit

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -1,21 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
+import {
+  MenuItem,
+  FormControl,
+  Select
+} from "@material-ui/core";
 
 function App() {
+  const[countries, setCountries] = useState(['USA', 'UK', 'INDIA']);
+
+
   return (
     <div className="app">
-      <h1>Lets build a COVID 19 tracker</h1>
+      <div className="app__header"> 
+      <h1>COVID-19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select
+            variant="outlined" value="abc">
+            {
+              countries.map(country => (
+                <MenuItem value={country}>{country}</MenuItem>
+              ))
+            }                
+
+            {/* <MenuItem value="worldwide">WorldWide</MenuItem>
+            <MenuItem value="worldwide">WorldWide</MenuItem>
+            <MenuItem value="worldwide">WorldWide</MenuItem>
+            <MenuItem value="worldwide">WorldWide</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
 
 
       {/* Header */}
-      {/* Title */}
+      {/* Title + Select input dropdown field */}
 
       {/* InfoBoxes */}
       {/* InfoBoxes */}
       {/* InfoBoxes */}
 
       {/* Table */}
-      
+      {/* Graph */}
+
       {/* Map */}
     </div>
   );
