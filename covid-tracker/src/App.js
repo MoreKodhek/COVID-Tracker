b43515a8a57c235539781674faca2b8c6875467b
MoreKodhek <morekodhek@gmail.com>
2020-11-12 12:13:29 +0300

    Banner started

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -1,13 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
-import {
-  MenuItem,
-  FormControl,
-  Select
-} from "@material-ui/core";
+import {MenuItem, FormControl, Select} from "@material-ui/core";
+import InfoBox from './InfoBox';
 
 function App() {
-  const[countries, setCountries] = useState(['USA', 'UK', 'INDIA']);
+  const[countries, setCountries] = useState([]);
+  const[country, setCountry] = useState('worldwide');
+
+  useEffect(() => {
+    const getCountriesData = async() => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => (
+          {
+            name: country.country,
+            value: country.countryInfo.iso2
+          }));
+
+          setCountries(countries); 
+      });
+    };
+
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value; 
+    setCountry(countryCode);
+  }
 
 
   return (
@@ -16,19 +37,20 @@ function App() {
       <h1>COVID-19 Tracker</h1>
         <FormControl className="app__dropdown">
           <Select
-            variant="outlined" value="abc">
-            {
-              countries.map(country => (
-                <MenuItem value={country}>{country}</MenuItem>
+            variant="outlined" onChange={onCountryChange}value={country}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map(country => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
               ))
             }                
-
-            {/* <MenuItem value="worldwide">WorldWide</MenuItem>
-            <MenuItem value="worldwide">WorldWide</MenuItem>
-            <MenuItem value="worldwide">WorldWide</MenuItem>
-            <MenuItem value="worldwide">WorldWide</MenuItem> */}
           </Select>
         </FormControl>
+      </div>
+
+      <div className="app__stats">
+        <InfoBox title="Coronavirus Cases" cases={123} total={2000}/>
+        <InfoBox title="Recovered" cases={1234} total={3000}/>
+        <InfoBox title="Deaths" cases={12345} total={4000}/>
       </div>
 
 
